7b72c96990865097aa7f8affb024c833df9e56d6
Juan Pablo Amorín <JUAN.AMORINJ@correo.ucu.edu.uy>
2024-05-20 20:29:52 -0300

    MyCheckbox

---
+++ ProyectoFinalGrupo_4/src/components/MyCheckbox/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function MyCheckbox( {text} ) {
+    return (
+    <div className="checkboxContainer">
+            <input type="checkbox" name={text} value={text}/> {text} 
+    </div>)
+}
+export default MyCheckbox
