eff1df264dc3ed6c31a813bc90d1924092acb1f7
IgnacioMartinez01 <igmartinezpintosgmail.com>
2024-05-22 19:13:45 -0300

    inputs

---
+++ ProyectoFinalGrupo_4/src/components/MyInputsEmailFront/MyInputsEmailFront.jsx
@@ -0,0 +1,11 @@
+import App from "../../App";
+
+function MyInputsEmailFront(){
+    return(
+        <div className="MyInputsEmailFront">
+            <input type="email" placeholder="Mail"></input>
+        </div>
+    );
+};
+
+export default MyInputsEmailFront;
